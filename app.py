de9269a6d214961a9b0296fe735ed1cd43f61712
Jacob Parks <jacob.m.parks02@gmail.com>
2026-09-14 15:46:41 -0500

    Added Greet Form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,6 +9,13 @@ def home():
 @app.route("/about")
 def about():
     return "<h1>About Page</h1><p>This is a simple Flask app I'm building.</p>"
+
+@app.route("/greet", methods=["GET", "POST"])
+def greet():
+    if request.method == "POST":
+        user_name = request.form.get("name")
+        return render_template("greet.html", name=user_name)
+    return render_template("greet_form.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
